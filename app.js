5fc5c0e8beb8c67a7ceb8c856c256101281347af
caixinjia <526113132@qq.com>
2017-11-08 18:05:45 +0800

    hbs

--- app.js
+++ app.js
@@ -4,16 +4,19 @@ var favicon = require('serve-favicon');
 var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
-
+// 引入handlebars
+var hbs = require('hbs');
 var index = require('./routes/index');
 var users = require('./routes/users');
 var account = require('./routes/account');
 var app = express();
 
 // view engine setup
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'jade');
-
+// app.set('views', path.join(__dirname, 'views'));
+// app.set('view engine', 'jade');
+app.set('view engine', 'hbs');
+app.set('views', __dirname + '/views');
+hbs.registerPartials(__dirname + '/views/partials');
 // uncomment after placing your favicon in /public
 //app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
 app.use(logger('dev'));
--- routes/account.js
+++ routes/account.js
@@ -3,7 +3,18 @@ var router = express.Router();
 router.get('/login', function(req, res) {
     return res.send('这里是登陆界面');
 })
-router.get('/register', function(req, res) {
-    return res.send('这里是注册界面');
-})
+router.route('/register')
+    // 返回注册页面
+    .get(function(req, res) {
+        res.render('account/register', { title: '注册' });
+    })
+    // 接受用户表单
+    .post(function(req, res, next) {
+        var username = req.body.username,
+            password = req.body.password;
+
+        console.log('Register post received!');
+        console.log('username:', username, 'password:', password);
+        res.end('成功收到POST请求');
+    });
 module.exports = router;
